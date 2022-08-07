c627c5c78f6fe831903d947c2e39c171e36bed34
RomaVgit <106352912+RomaVgit@users.noreply.github.com>
2022-08-07 22:24:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+"use strict"
+
+const reviewsSwiper = document.querySelector('.swiper');
+
+if (reviewsSwiper) {
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  autoHeight: true,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
+}
